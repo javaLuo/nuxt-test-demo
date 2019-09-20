a013d5a821843dca45cacdd4b2d5e0a1d2bf0f73
L <376693576@qq.com>
2019-09-20 11:21:32 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,10 @@
 const pkg = require('./package')
-const TerserPlugin = require('terser-webpack-plugin')
 module.exports = {
   mode: 'universal',
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
@@ -16,57 +15,42 @@ module.exports = {
   },
 
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
 
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: ['element-ui/lib/theme-chalk/index.css'],
 
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: ['@/plugins/element-ui'],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios'
   ],
   /*
-  ** Axios module configuration
-  */
+   ** Axios module configuration
+   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    publicPath: '/right/',
+     ** You can extend webpack config here
+     */
     extractCSS: { allChunks: true },
-    // optimization: {
-    //   minimizer: [
-    //     new TerserPlugin({
-    //       terserOptions: {
-    //         compress: {
-    //           warnings: false,
-    //           // drop_console: true,
-    //           drop_debugger: true,
-    //           pure_funcs: ['console.log']
-    //         }
-    //       }
-    //     })
-    //   ]
-    // },
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
