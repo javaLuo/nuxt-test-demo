ccd6d2d521f94c5339c6cd7deeca75f6991f67dd
L <376693576@qq.com>
2019-09-20 10:53:35 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nuxt-test-demo
 测试nuxt2.9.x,疑似有个小BUG
+
+<hr/>
+
+在`nuxt.config.js`中设置了allChunks后
+```
+  build:{
+    extractCSS: {allChunks: true}
+  }
+```
+每次`yarn build` 生成的文件hash都不一样
---
+++ nuxt.config.js
@@ -0,0 +1,82 @@
+const pkg = require('./package')
+const TerserPlugin = require('terser-webpack-plugin')
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: ['element-ui/lib/theme-chalk/index.css'],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: ['@/plugins/element-ui'],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    publicPath: '/right/',
+    extractCSS: { allChunks: true },
+    // optimization: {
+    //   minimizer: [
+    //     new TerserPlugin({
+    //       terserOptions: {
+    //         compress: {
+    //           warnings: false,
+    //           // drop_console: true,
+    //           drop_debugger: true,
+    //           pure_funcs: ['console.log']
+    //         }
+    //       }
+    //     })
+    //   ]
+    // },
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
